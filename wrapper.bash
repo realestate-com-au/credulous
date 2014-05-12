7db687ab5d4b49e10533aec8cdfd43f4f23d5e39
Mike Bailey <mike.bailey@rea-group.com>
2014-05-13 00:22:27 +1000

    Pass command line args to credulous
    User may want to specify account or username

--- wrapper.bash
+++ wrapper.bash
@@ -8,9 +8,15 @@ cred () {
         echo "GOBIN is not set"
         return 1
     fi
-    RES=$( $GOBIN/credulous source )
+    RES=$( $GOBIN/credulous source $@)
     if [ $? -eq 0 ]; then
+        echo -n "Loading AWS creds into current environment..."
         eval "$RES"
+        if [ $? -eq 0 ]; then
+          echo "OK"
+        else
+          echo "FAIL"
+        fi
     else
         echo "Failed to source credentials"
         return 1
