a2947b948471428eae8b1237f012654f98e1f288
Colin Panisset <colin.panisset@rea-group.com>
2014-06-26 20:05:07 +1000

    Add git commits and chec if a dir is a repo

--- git.go
+++ git.go
@@ -1,30 +1,100 @@
 package main
 
-import "github.com/libgit2/git2go"
+import (
+	"path"
+	"time"
 
-func gitAdd(repository, filename string) (err error) {
-	repo, err := git.OpenRepository(repository)
+	"github.com/libgit2/git2go"
+)
+
+type RepoConfig struct {
+	Name  string
+	Email string
+}
+
+func isRepo(checkpath string) (bool, error) {
+	ceiling := []string{checkpath}
+
+	repopath, err := git.Discover(checkpath, false, ceiling)
 	if err != nil {
-		return err
+		return false, err
+	}
+	// the path is the parent of the repo, which appends '.git'
+	// to the path
+	dirpath := path.Dir(path.Clean(repopath))
+	if dirpath == checkpath {
+		return true, nil
+	}
+	return false, nil
+}
+
+func getRepoConfig(repo *git.Repository) (RepoConfig, error) {
+	config, err := repo.Config()
+	if err != nil {
+		return RepoConfig{}, err
+	}
+	name, err := config.LookupString("user.name")
+	if err != nil {
+		return RepoConfig{}, err
+	}
+	email, err := config.LookupString("user.email")
+	if err != nil {
+		return RepoConfig{}, err
+	}
+	repoconf := RepoConfig{
+		Name:  name,
+		Email: email,
+	}
+	return repoconf, nil
+}
+
+func gitAddCommitFile(repopath, filename, message string) (commitId string, err error) {
+	repo, err := git.OpenRepository(repopath)
+	if err != nil {
+		return "", err
+	}
+
+	config, err := getRepoConfig(repo)
+	if err != nil {
+		return "", err
 	}
 
 	index, err := repo.Index()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	err = index.AddByPath(filename)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	err = index.Write()
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
-}
 
-func gitCommit(repo, comment string) (err error) {
-	return nil
+	treeId, err := index.WriteTree()
+	if err != nil {
+		return "", err
+	}
+
+	// new file is now staged, so we have to create a commit
+	sig := &git.Signature{
+		Name:  config.Name,
+		Email: config.Email,
+		When:  time.Now(),
+	}
+
+	tree, err := repo.LookupTree(treeId)
+	if err != nil {
+		return "", err
+	}
+
+	commit, err := repo.CreateCommit("HEAD", sig, sig, message, tree)
+	if err != nil {
+		return "", err
+	}
+
+	return commit.String(), nil
 }
--- git_test.go
+++ git_test.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+
+	"github.com/libgit2/git2go"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGitAdd(t *testing.T) {
+	Convey("Testing gitAdd", t, func() {
+		Convey("Test add to non-existent repository", func() {
+			_, err := gitAddCommitFile("/no/such/repo", "testdata/newcreds.json", "message")
+			So(err, ShouldNotEqual, nil)
+		})
+
+		// need to create a new repo first
+		repopath := path.Join("testrepo." + fmt.Sprintf("%d", os.Getpid()))
+		repo, err := git.InitRepository(repopath, false)
+		panic_the_err(err)
+
+		Convey("Test add non-existent file to a repo", func() {
+			_, err := gitAddCommitFile(repo.Path(), "/no/such/file", "message")
+			// fmt.Println("gitAdd returned " + fmt.Sprintf("%s", err))
+			So(err, ShouldNotEqual, nil)
+		})
+
+		Convey("Test add a file to the repo", func() {
+			fp, _ := os.Create(path.Join(repopath, "testfile"))
+			_, _ = fp.WriteString("A test string")
+			_ = fp.Close()
+			commitId, err := gitAddCommitFile(repo.Path(), "testfile", "message")
+			So(err, ShouldEqual, nil)
+			So(commitId, ShouldNotEqual, nil)
+			So(commitId, ShouldNotBeBlank)
+		})
+
+		Convey("Test checking whether a repo is a repo", func() {
+			fullpath, _ := filepath.Abs(repopath)
+			isrepo, err := isRepo(fullpath)
+			So(err, ShouldEqual, nil)
+			So(isrepo, ShouldEqual, true)
+		})
+
+		// os.RemoveAll(path.Clean(path.Join(repo.Path(), "..")))
+
+	})
+}
